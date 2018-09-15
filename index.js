5a8b1bd1d059edab201271410be91719cd7f0dc4
jacky.li <jacky.li@accenture.com>
2018-09-15 11:36:05 +0800

    update code structure

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import {AppRegistry} from 'react-native';
 
 import {name as appName} from './app.json';
 
-import App from './dapp/test/app';
-// import App from './dapp/main/app';
+// import App from './dapp/test/app';
+import App from './src/app';
 // console.log(App);
 AppRegistry.registerComponent(appName, () => App);
 
