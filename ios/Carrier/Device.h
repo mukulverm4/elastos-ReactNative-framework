9962f82fbaee0daeb8c65b4e284b327f75c9f0c4
jacky.li <jacky.li@accenture.com>
2018-09-24 15:46:28 +0800

    add carrier framework

---
+++ ios/Carrier/Device.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import <ElastosCarrier/ElastosCarrier.h>
+
+#define kNotificationDeviceConnected        @"kNotificationDeviceConnected"
+#define kNotificationDeviceConnectFailed    @"kNotificationDeviceConnectFailed"
+
+@interface Device : NSObject
+
+
+
+@end
---
+++ ios/Carrier/Device.m
@@ -0,0 +1,17 @@
+#import "Device.h"
+
+
+static NSString * const KEY_LocalPort = @"localPort";
+
+@interface Device ()
+{
+ 
+}
+
+@end
+
+@implementation Device
+
+
+
+@end
