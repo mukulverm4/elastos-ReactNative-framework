adbdf51a54122cca24c1f88cef824de71920ae8c
jacky.li <jacky.li@accenture.com>
2018-10-11 07:14:33 +0800

    revert framework

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {AppRegistry} from 'react-native';
 
 
-import './dapp/carrier_demo/app.bak';
+import './dapp/carrier_demo/app';
 
 // import App from './src/main';
 // AppRegistry.registerComponent('ELASTOS_RN_FRAMEWORK', () => App);
